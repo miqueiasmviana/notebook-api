cd1ac455a4f0eba01b8fd5a70f319efbf58e8b00
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-22 16:39:20 -0300

    Gerando segundo CRUD e relacionando db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_122036) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_193743) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.date "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "kinds", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
