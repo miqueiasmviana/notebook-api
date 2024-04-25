c3da774335b571b198f5a28bc4e9dfe4430604f8
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-25 16:54:42 -0300

    before_action :ensure_json_resquest. Adicionar Serializer de phones e addresses

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,17 +2,18 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate #, :author
 
   belongs_to :kind do
-    link(:kind) {kind_url(object.kind.id)}
+    link(:related) {kind_url(object.kind.id)}
   end
+  
   has_many :phones
   has_one :address
 
-  link(:self) {contact_url(object.id)}
-  link(:kind) {kind_url(object.kind.id)}
+  # link(:self) {contact_url(object.id)}
+  # link(:kind) {kind_url(object.kind.id)}
 
 
   meta do
-    {author: "Mikaelly Viana"}
+    {author: "Eduardo Salgueiro"}
   end
 
   def attributes(*args)
