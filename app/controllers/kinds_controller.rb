01cbf8108358454cbb3c8602cd9fce8a785cb1e3
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-04-30 13:01:02 -0300

    adicionando before action authenticate_user. Desativando include ActionController e authenticate

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,8 +1,9 @@
 class KindsController < ApplicationController
 
+  before_action :authenticate_user!
   # TOKEN = "secret123"
-  include ActionController::HttpAuthentication::Token::ControllerMethods
-
+  # include ActionController::HttpAuthentication::Token::ControllerMethods
+  
   # include ActionController::HttpAuthentication::Basic::ControllerMethods
   # http_basic_authenticate_with name: "miqueias", password: "secret"
   # include ActionController::HttpAuthentication::Digest::ControllerMethods
@@ -64,20 +65,20 @@ class KindsController < ApplicationController
       params.require(:kind).permit(:description)
     end
 
-    def authenticate
+    # def authenticate
       # authenticate_or_request_with_http_digest("Application") do |username|
       #   USERS[username]
       # end
       
-      authenticate_or_request_with_http_token do |token, options|
-        hmac_secret = 'my$ecretK3y'
-        JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+      # authenticate_or_request_with_http_token do |token, options|
+      #   hmac_secret = 'my$ecretK3y'
+      #   JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
         # Compare the tokens in a time-constant manner, to mitigate
         # timing attacks.
         # ActiveSupport::SecurityUtils.secure_compare(
         # ::Digest::SHA256.hexdigest(token),
         # ::Digest::SHA256.hexdigest(TOKEN)
         # )
-      end
-    end
+    #   end
+    # end
 end
