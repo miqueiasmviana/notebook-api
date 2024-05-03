e73e7d8be58249f4245a50b5131ebf46d3b3a2e0
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-05-03 11:03:56 -0300

    adicionando validações

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,13 @@
 class Contact < ApplicationRecord
 
+  validates_presence_of :kind
+  validates_presence_of :address
+
 
   #kaminari
   paginates_per 5
 
-  belongs_to :kind, optional: true
+  belongs_to :kind #, optional: true
   has_many :phones
   has_one :address
 
